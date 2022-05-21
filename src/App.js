d099a5f49d0ed150bb32498e360ce15683ed2fd2
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-05-21 23:23:18 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Footer from "../src/Components/Footer/Footer";
 import "./App.css";
 import Home from "./View/Home/Home";
+import React from "react";
 
 function App() {
-  return (
-    <>
-      <Home></Home>
-      <Footer></Footer>
-    </>
-  );
+    return (
+        <>
+            <Home/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
