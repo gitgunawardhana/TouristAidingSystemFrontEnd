2a3efcd54000f8c6bbf9e2a9f2be98aaf2554475
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-05-28 13:25:08 +0530

    Added relevant files for Accommodation page

--- src/View/Accommodation/Accommodation.js
+++ src/View/Accommodation/Accommodation.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "../../Components/NavigationBar/Navbar/Navbar";
 import "./Accommodation.css";
+import BackgroundImageSet from "../../Components/Accommodation/BackgroundImageSet/BackgroundImageSet";
 
 function Test() {
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  );
+    return (
+        <>
+            <Navbar></Navbar>
+            <BackgroundImageSet></BackgroundImageSet>
+
+        </>
+    );
 }
 
 export default Test;
