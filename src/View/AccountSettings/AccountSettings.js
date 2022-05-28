427c1eef63ff97ff83dfc2ff7c39c276677c86df
PrimeZien <primezien@gmail.com>
2022-05-28 14:22:40 +0530

    Added file

---
+++ src/View/AccountSettings/AccountSettings.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../Components/NavigationBar/Navbar/Navbar";
+import "./AccountSettings.css";
+import Body from "../../Components/AccountSettings/AccountSettings";
+
+function AccountSetting() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Body></Body>
+        </>
+    );
+}
+
+export default AccountSetting;
