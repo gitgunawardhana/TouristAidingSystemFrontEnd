f57827997ba84d66dd97701f021f2aea05c7cb4f
PrimeZien <primezien@gmail.com>
2022-06-20 22:10:40 +0530

    added changes

--- src/Component/SignInOrCreateAcconunt/SignUp.js
+++ src/Component/SignInOrCreateAcconunt/SignUp.js
@@ -10,7 +10,7 @@ function SignInComponent() {
             <ul class="nav nav-pills nav-justified mb-3 " id="ex1" role="tablist">
                 <li class="nav-item" role="presentation">
                     <a
-                        class="nav-link active"
+                        class="nav-link active  nav-bar-link"
                         id="tab-login"
                         data-toggle="pill"
                         href="#pills-login"
