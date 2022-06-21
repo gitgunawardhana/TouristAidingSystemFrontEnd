8821e2728407cae9ced600361fd6527e0b8a5449
PrimeZien <primezien@gmail.com>
2022-06-22 00:44:45 +0530

    added sign up page and change nav bar and Footer

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -67,98 +67,112 @@ function Navbar() {
 
 
                         {/* <!-- Right elements --> */}
-                        <div className="d-flex align-items-center">
-                            {/* <!-- Icon --> */}
-                            {/*<Link className="text-reset me-3" to="#">*/}
-                            {/*  <i className="fas fa-shopping-cart"></i>*/}
-                            {/*</a>*/}
-
-                            {/* <!-- Notifications --> */}
-                            <div className="dropdown">
-                                <a
-                                    className="text-reset me-3 dropdown-toggle align-items-center hidden-arrow"
-                                    to="#"
-                                    id="navbarDropdownMenuLink"
-                                    role="button"
-                                    data-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    <i className="fas fa-bell"></i>
-                                    <span className="badge rounded-pill badge-notification bg-danger">
-                    1
-                  </span>
-
-                                </a>
-                                <ul
-                                    className="dropdown-menu dropdown-menu-end ul-item"
-                                    aria-labelledby="navbarDropdownMenuLink"
-                                >
-                                    <div className="drop-item">
-                                    <li>
-                                        <Link className="dropdown-item" to="#">
-                                            Some news
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link className="dropdown-item" to="#">
-                                            Another news
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link className="dropdown-item" to="#">
-                                            Something else here
-                                        </Link>
-                                    </li>
-                                    </div>
-                                </ul>
-                            </div>
-
-                            {/* <!-- Avatar --> */}
-                            <div className="dropdown new-div" >
-                                <a
-                                    className="tdropdown-toggle d-flex align-items-center hidden-arrow"
-                                    to="#"
-                                    id="navbarDropdownMenuLink"
-                                    role="button"
-                                    data-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    <img
-                                        src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                                        className="rounded-circle"
-                                        height="25"
-                                        alt="Black and White Portrait of a Man"
-                                        loading="lazy"
-                                    />
-                                </a>
+                        {/*      <div className="d-flex align-items-center">*/}
 
 
+                        {/*          /!* <!-- Notifications --> *!/*/}
+                        {/*          <div className="dropdown">*/}
+                        {/*              <a*/}
+                        {/*                  className="text-reset me-3 dropdown-toggle align-items-center hidden-arrow"*/}
+                        {/*                  to="#"*/}
+                        {/*                  id="navbarDropdownMenuLink"*/}
+                        {/*                  role="button"*/}
+                        {/*                  data-toggle="dropdown"*/}
+                        {/*                  aria-expanded="false"*/}
+                        {/*              >*/}
+                        {/*                  <i className="fas fa-bell"></i>*/}
+                        {/*                  <span className="badge rounded-pill badge-notification bg-danger">*/}
+                        {/*  1*/}
+                        {/*</span>*/}
 
-                                    <ul
-                                        className="dropdown-menu dropdown-menu-end ul-item"
-                                        aria-labelledby="navbarDropdownMenuAvatar"
-                                    >
-                                        <div className="drop-item">
-                                            <li>
-                                                <Link className="dropdown-item" to="/profile">
-                                                    My profile
-                                                </Link>
-                                            </li>
-                                            <li>
-                                                <Link className="dropdown-item" to="/accountSetting">
-                                                    Settings
-                                                </Link>
-                                            </li>
-                                            <li>
-                                                <Link className="dropdown-item" to="logout">
-                                                    Logout
-                                                </Link>
-                                            </li>
-                                        </div>
-                                    </ul>
-                                </div>
-                        </div>
+                        {/*              </a>*/}
+                        {/*              <ul*/}
+                        {/*                  className="dropdown-menu dropdown-menu-end ul-item"*/}
+                        {/*                  aria-labelledby="navbarDropdownMenuLink"*/}
+                        {/*              >*/}
+                        {/*                  <div className="drop-item">*/}
+                        {/*                  <li>*/}
+                        {/*                      <Link className="dropdown-item" to="#">*/}
+                        {/*                          Some news*/}
+                        {/*                      </Link>*/}
+                        {/*                  </li>*/}
+                        {/*                  <li>*/}
+                        {/*                      <Link className="dropdown-item" to="#">*/}
+                        {/*                          Another news*/}
+                        {/*                      </Link>*/}
+                        {/*                  </li>*/}
+                        {/*                  <li>*/}
+                        {/*                      <Link className="dropdown-item" to="#">*/}
+                        {/*                          Something else here*/}
+                        {/*                      </Link>*/}
+                        {/*                  </li>*/}
+                        {/*                  </div>*/}
+                        {/*              </ul>*/}
+                        {/*          </div>*/}
+
+                        {/*          /!* <!-- Avatar --> *!/*/}
+                        {/*          <div className="dropdown new-div" >*/}
+                        {/*              <a*/}
+                        {/*                  className="tdropdown-toggle d-flex align-items-center hidden-arrow"*/}
+                        {/*                  to="#"*/}
+                        {/*                  id="navbarDropdownMenuLink"*/}
+                        {/*                  role="button"*/}
+                        {/*                  data-toggle="dropdown"*/}
+                        {/*                  aria-expanded="false"*/}
+                        {/*              >*/}
+                        {/*                  <img*/}
+                        {/*                      src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"*/}
+                        {/*                      className="rounded-circle"*/}
+                        {/*                      height="25"*/}
+                        {/*                      alt="Black and White Portrait of a Man"*/}
+                        {/*                      loading="lazy"*/}
+                        {/*                  />*/}
+                        {/*              </a>*/}
+
+
+                        {/*                  <ul*/}
+                        {/*                      className="dropdown-menu dropdown-menu-end ul-item"*/}
+                        {/*                      aria-labelledby="navbarDropdownMenuAvatar"*/}
+                        {/*                  >*/}
+                        {/*                      <div className="drop-item">*/}
+                        {/*                          <li>*/}
+                        {/*                              <Link className="dropdown-item" to="/profile">*/}
+                        {/*                                  My profile*/}
+                        {/*                              </Link>*/}
+                        {/*                          </li>*/}
+                        {/*                          <li>*/}
+                        {/*                              <Link className="dropdown-item" to="/accountSetting">*/}
+                        {/*                                  Settings*/}
+                        {/*                              </Link>*/}
+                        {/*                          </li>*/}
+                        {/*                          <li>*/}
+                        {/*                              <Link className="dropdown-item" to="logout">*/}
+                        {/*                                  Logout*/}
+                        {/*                              </Link>*/}
+                        {/*                          </li>*/}
+                        {/*                      </div>*/}
+                        {/*                  </ul>*/}
+                        {/*              </div>*/}
+                        {/*      </div>*/}
                         {/* <!-- Right elements --> */}
+
+                        <div className="d-flex align-items-center sign-btn-set">
+                            <Link to="/signUp">
+                                <button type="button" className="btn btn-primary  sign-up-btn">
+                                    Sign up for free
+                                </button>
+                            </Link>
+
+                            <Link to="/signUp">
+                                <button type="button" className="btn btn-primary  px-3 me-2 login-btn">
+                                    Login
+                                </button>
+                            </Link>
+
+
+                        </div>
+
+
                     </div>
                     {/* <!-- Container wrapper --> */}
                 </nav>
--- src/Component/SignInOrCreateAcconunt/SignUp.js
+++ src/Component/SignInOrCreateAcconunt/SignUp.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./SignUp.css"
+import {useState} from 'react';
 import Navbar from "../Navbar/Navbar";
+import {Button} from "bootstrap";
 
 function SignInComponent() {
+    const [agree, setAgree] = useState(false);
+
+    const checkboxHandler = () => {
+        // if agree === true, it will be set to false
+        // if agree === false, it will be set to true
+        setAgree(!agree);
+        // Don't miss the exclamation mark
+    }
+
+    // When the button is clicked
+    const btnHandler = () => {
+        alert('The buttion is clickable!');
+    };
+
+
     return (
-            <div className="shadow-lg p-3 mb-5 bg-white rounded main-div">
+        <div className="shadow-lg p-3 mb-5 bg-white rounded main-div">
 
 
             <ul class="nav nav-pills nav-justified mb-3 " id="ex1" role="tablist">
@@ -29,6 +46,7 @@ function SignInComponent() {
                         role="tab"
                         aria-controls="pills-register"
                         aria-selected="false"
+
                     >Register</a
                     >
                 </li>
@@ -60,13 +78,13 @@ function SignInComponent() {
 
 
                         <div class="form-outline mb-4">
-                            <input type="email" id="loginName" class="form-control" />
+                            <input type="email" id="loginName" class="form-control"/>
                             <label class="form-label" for="loginName">Email or username</label>
                         </div>
 
 
                         <div class="form-outline mb-4">
-                            <input type="password" id="loginPassword" class="form-control" />
+                            <input type="password" id="loginPassword" class="form-control"/>
                             <label class="form-label" for="loginPassword">Password</label>
                         </div>
 
@@ -75,7 +93,7 @@ function SignInComponent() {
                             <div class="col-md-6 d-flex justify-content-center">
 
                                 <div class="form-check mb-3 mb-md-0">
-                                    <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked />
+                                    <input class="form-check-input" type="checkbox" value="" id="loginCheck"/>
                                     <label class="form-check-label" for="loginCheck"> Remember me </label>
                                 </div>
                             </div>
@@ -89,10 +107,17 @@ function SignInComponent() {
                         <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
 
 
-                        <div class="text-center">
-                            <p>Not a member? <a href="#!">Register</a></p>
+                        <div class="text-center" >
+                            <p>Not a member?
+
+                                <a data-toggle="pill"
+                                   href="#pills-register"
+
+                                >Register</a></p>
                         </div>
                     </form>
+
+
                 </div>
                 <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
                     <form>
@@ -120,33 +145,34 @@ function SignInComponent() {
 
                         <div class="form-outline mb-4">
                             <label className="form-label" htmlFor="registerName">Name</label>
-                            <input type="text" id="registerName" class="form-control" placeholder="Name" />
+                            <input type="text" id="registerName" class="form-control" placeholder="Name"/>
 
                         </div>
 
 
                         <div class="form-outline mb-4">
                             <label className="form-label" htmlFor="registerUsername">Username</label>
-                            <input type="text" id="registerUsername" class="form-control" placeholder="Username" />
+                            <input type="text" id="registerUsername" class="form-control" placeholder="Username"/>
 
                         </div>
 
 
                         <div class="form-outline mb-4">
                             <label className="form-label" htmlFor="registerEmail">Email</label>
-                            <input type="email" id="registerEmail" class="form-control" placeholder="Email" />
+                            <input type="email" id="registerEmail" class="form-control" placeholder="Email"/>
 
                         </div>
 
                         <div class="form-outline mb-4">
                             <label className="form-label" htmlFor="registerPassword">Password</label>
-                            <input type="password" id="registerPassword" class="form-control" placeholder="Password" />
+                            <input type="password" id="registerPassword" class="form-control" placeholder="Password"/>
 
                         </div>
 
                         <div class="form-outline mb-4">
                             <label className="form-label" htmlFor="registerRepeatPassword">Repeat password</label>
-                            <input type="password" id="registerRepeatPassword" class="form-control" placeholder="Repeat password" />
+                            <input type="password" id="registerRepeatPassword" class="form-control"
+                                   placeholder="Repeat password"/>
 
                         </div>
 
@@ -156,22 +182,23 @@ function SignInComponent() {
                                 class="form-check-input me-2"
                                 type="checkbox"
                                 value=""
-                                id="registerCheck"
-                                checked
+                                id="agree"
                                 aria-describedby="registerCheckHelpText"
+                                onChange={checkboxHandler}
                             />
                             <label class="form-check-label" for="registerCheck">
                                 I have read and agree to the terms
                             </label>
                         </div>
 
+                        <button type="submit" className="btn btn-primary btn-block mb-3" disabled={!agree}
+                                onClick={btnHandler}>Sign up
+                        </button>
 
-                        <button type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
                     </form>
                 </div>
             </div>
-
-
+            <div className="last-div"></div>
 
         </div>
     );
