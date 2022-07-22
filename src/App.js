14b51dd0f744651c56a144430af3c52dfa6bbf43
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-23 02:57:38 +0530

    Optimized code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import "./App.css";
-import Footer from "./Component/Footer/Footer";
 import ScrollToTop from "./Component/ScrollToTop";
 import Accommodation from "./View/Accommodation/Accommodation";
 import AccommodationFilter from "./View/AccommodationFilter/AccommodationFilter";
@@ -26,45 +25,46 @@ import TripPlanner from "./View/TripPlanner/TripPlanner";
 import ForgottenPassword from "./View/ForgottenYourPassword/ForgottenYourPassword";
 import CreateNewPassword from "./View/CreateNewPassword/CreateNewPassword";
 import CheckYourEmail from "./View/CheckYourEmail/CheckYourEmail";
+
 function App() {
-  return (
-    <>
-      <Router>
-        <ScrollToTop></ScrollToTop>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/locations" element={<Location />} />
-          <Route path="/accommodations" element={<Accommodation />} />
-          <Route
-            path="/accommodations-filter"
-            element={<AccommodationFilter />}
-          />
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="/security" element={<Security />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route
-            path="/notificationSettings"
-            element={<NotificationSettings />}
-          />
-          <Route path="/paymentMethod" element={<PaymentMethod />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/location" element={<Locations />} />
-          <Route path="/location/:id" element={<Location />} />
-          <Route path="/attraction/:id" element={<Attraction />} />
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="/createAccount" element={<CreateAccount />} />
-          <Route path="/emailVerification" element={<EmailVerification />} />
-          <Route path="/vehicles" element={<Vehicle />} />
-          <Route path="/tripPlanner" element={<TripPlanner/>}/>
-          <Route path="/forgetPassword" element={<ForgottenPassword/>}/>
-          <Route path="/createNewPassword" element={<CreateNewPassword/>}/>
-          <Route path="/checkYourEmail" element={<CheckYourEmail/>}/>
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <ScrollToTop></ScrollToTop>
+                <Routes>
+                    <Route path="/" exact element={<Home/>}/>
+                    <Route path="/locations" element={<Location/>}/>
+                    <Route path="/accommodations" element={<Accommodation/>}/>
+                    <Route
+                        path="/accommodations-filter"
+                        element={<AccommodationFilter/>}
+                    />
+                    <Route path="*" element={<PageNotFound/>}/>
+                    <Route path="/security" element={<Security/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/signUp" element={<SignUp/>}/>
+                    <Route
+                        path="/notificationSettings"
+                        element={<NotificationSettings/>}
+                    />
+                    <Route path="/paymentMethod" element={<PaymentMethod/>}/>
+                    <Route path="/notifications" element={<Notifications/>}/>
+                    <Route path="/signIn" element={<SignIn/>}/>
+                    <Route path="/location" element={<Locations/>}/>
+                    <Route path="/location/:id" element={<Location/>}/>
+                    <Route path="/attraction/:id" element={<Attraction/>}/>
+                    <Route path="*" element={<PageNotFound/>}/>
+                    <Route path="/createAccount" element={<CreateAccount/>}/>
+                    <Route path="/emailVerification" element={<EmailVerification/>}/>
+                    <Route path="/vehicles" element={<Vehicle/>}/>
+                    <Route path="/tripPlanner" element={<TripPlanner/>}/>
+                    <Route path="/forgetPassword" element={<ForgottenPassword/>}/>
+                    <Route path="/createNewPassword" element={<CreateNewPassword/>}/>
+                    <Route path="/checkYourEmail" element={<CheckYourEmail/>}/>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
