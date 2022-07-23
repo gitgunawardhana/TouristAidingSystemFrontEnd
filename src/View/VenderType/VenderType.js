d3c979a175c551122652d8c764feb72050e67ecc
ishan <t.gunawardana864@gmail.com>
2022-07-24 03:04:32 +0530

    Created VenderType Page

---
+++ src/View/VenderType/VenderType.js
@@ -0,0 +1,11 @@
+import React from "react";
+import VenderType from "../../Component/VenderType/VenderType";
+
+function SignIn() {
+  return (
+    <>
+      <VenderType />
+    </>
+  );
+}
+export default SignIn;
