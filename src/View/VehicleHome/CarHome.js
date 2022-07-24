a65c842f48e8a58f4baf335a53b54c765bd07d7a
Sanidu-Udawatta <saniduhasanka@gmail.com>
2022-07-24 12:41:04 +0530

    updated files

--- src/View/VehicleHome/CarHome.js
+++ src/View/VehicleHome/CarHome.js
@@ -2,13 +2,13 @@ import React from 'react'
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
 import Hero from '../../Component/Vehicle_Home_Hero/Hero.js';
-import Find from '../../Component/Car_Home_Carousel/Find.js';
+import Find from '../../Component/CarHomeCarousel/Find.js';
 import Register from "../../Component/Register/Register.js";
-import Faq from "../../Component/Car_Home_FAQ_section/Faq.js";
+import Faq from "../../Component/CarHomeFAQSection/Faq.js";
 import Footer from "../../Component/Footer/Footer.js";
 import Navbar from "../../Component/Navbar/Navbar.js"
 
-const Car_Home = () => {
+const CarHome = () => {
   return (
     <>
       <Navbar />
@@ -21,4 +21,4 @@ const Car_Home = () => {
   )
 }
 
-export default Car_Home
+export default CarHome
